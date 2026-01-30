e2985a101dd41f6dc52b29f7a1c80e40bf6d125a
Swenschaeferjohann <swen@lightprotocol.com>
2026-01-30 15:28:34 +0000

    devnet default

--- typescript-client/actions/transfer-interface.ts
+++ typescript-client/actions/transfer-interface.ts
@@ -12,10 +12,10 @@ import { homedir } from "os";
 import { readFileSync } from "fs";
 
 // devnet:
-// const RPC_URL = `https://devnet.helius-rpc.com?api-key=${process.env.API_KEY!}`;
-// const rpc = createRpc(RPC_URL);
+const RPC_URL = `https://devnet.helius-rpc.com?api-key=${process.env.API_KEY!}`;
+const rpc = createRpc(RPC_URL);
 // localnet:
-const rpc = createRpc();
+// const rpc = createRpc();
 
 const payer = Keypair.fromSecretKey(
     new Uint8Array(
